c72f8dd2aefe639fe58837c6d9fb0f42212d77d8
Alexey Solovyov <solovyev@happy-job.ru>
2019-06-03 12:16:11 +0300

    Router History Mode: first  steps

--- router.js
+++ router.js
@@ -2,14 +2,26 @@ import RouterOutlet from "./RouterOutlet.svelte" // rollup
 import RouterOutlet from "!!svelte-loader!./RouterOutlet.svelte" // webpack
 
 class Router {
+
   constructor (routes) {
+    if (!routes || typeof routes != 'object') {
+      console.error('Wrong parameters given to Router constructor')
+      return false
+    }
+    this.mode = 'hash' // TODO: add 'history' mode
     this.routes = routes.routes
     this.currentRouteComponent = routes.main
     this.afterUpdate = routes.callback
-    window.onhashchange = this.parseHash.bind(this)
+    if (this.mode == 'hash') window.onhashchange = this.parseHash.bind(this)
+    if (this.mode == 'history') window.onpopstate = this.parseHash.bind(this)
   }
 
   createOutlet () {
+    let outletDiv = document.getElementById('router-outlet')
+    if (!outletDiv) {
+      console.error('Could not find element with id "router-outlet". Router NOT created...')
+      return false
+    }
     let outlet = new RouterOutlet({
       target: document.getElementById('router-outlet'),
       props: {
@@ -28,25 +40,40 @@ class Router {
   }
 
   parseHash () {
-    let hash = window.location.hash.replace('#', '')
-    var routeArray = hash.split('?')
-    var routeInfo = {}
-    routeInfo['fullPath'] = routeArray[0]
-    routeInfo['route'] = routeArray[0].split('/')
-    routeInfo['query'] = {}
-    if (routeArray[1]) {
-      var routeQuery = routeArray[1].split('&')
-      routeQuery.forEach((param) => {
-        let keyValue = param.split('=')
-        routeInfo['query'][keyValue[0]] = keyValue[1]
-      })
+    if (this.mode == 'hash') {
+      if (window.location.hash == "") {
+        this.push('/')
+      }
+      let hash = window.location.hash.replace('#', '')
+      var routeArray = hash.split('?')
+      var routeInfo = {}
+      routeInfo['fullPath'] = routeArray[0]
+      routeInfo['route'] = routeArray[0].split('/')
+      routeInfo['query'] = {}
+      if (routeArray[1]) {
+        var routeQuery = routeArray[1].split('&')
+        routeQuery.forEach((param) => {
+          let keyValue = param.split('=')
+          routeInfo['query'][keyValue[0]] = keyValue[1]
+        })
+      }
+      this.currentRoute = routeInfo
+      this.compareRoutes()
     }
-    this.currentRoute = routeInfo
-    this.compareRoutes()
+    else if (this.mode == 'history') {
+      if (history.state == null) {
+        this.push('/')
+      }
+      console.log(history.state)
+    }
   }
 
   push (url) {
-    window.location.hash = url
+    if (this.mode == 'hash') window.location.hash = url
+    if (this.mode == 'history') {
+      let stateObj = { path: "/" };
+      history.pushState(stateObj, "", "/");
+    }
   }
 
   get getCurrentRoute () {
