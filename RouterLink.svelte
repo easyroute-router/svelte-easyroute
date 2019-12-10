521fd8ae7aaa08df885dfd789ace7c188b47d7aa
Alexey Solovyov <plotinka@lyoha.info>
2019-12-10 21:44:28 +0300

    Fixed outlet component; RouterLink component

---
+++ RouterLink.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let to
+
+  function navigate (evt) {
+    evt.preventDefault()
+    evt.stopPropagation()
+    window.location.hash = `#${to}`
+  }
+</script>
+
+<a href="{to}" on:click={(evt) => {navigate(evt)}}>
+  <slot></slot>
+</a>
--- RouterOutlet.svelte
+++ RouterOutlet.svelte
@@ -1,16 +1,19 @@
 <script>
   export let router;
   let _routeComponent = false;
+  let _routeInfo = {};
   router.afterUpdate = () => {
     console.log(router.currentRoute);
     _routeComponent = false;
     setTimeout(() => {
       _routeComponent = router.currentRoute.routeObject.component
+      _routeInfo = router.currentRoute.routeInfo
     },2)
   }
 
   $: routeComponent = _routeComponent
+  $: routeInfo = _routeInfo
 </script>
 
-<svelte:component this={routeComponent}/>
+<svelte:component this={routeComponent} currentRoute={routeInfo}/>
 
