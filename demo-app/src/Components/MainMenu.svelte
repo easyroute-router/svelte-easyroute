34eccd51b3ce4ac02d8f211057dacfa9e2ddbe47
Alexey Solovyov <plotinka@lyoha.info>
2020-06-18 09:36:13 +0300

    17.06 Menu to external component

---
+++ demo-app/src/Components/MainMenu.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { RouterLink } from '../../../lib'
+</script>
+
+<ul class="uk-nav uk-nav-default">
+    <li class="uk-active">
+        <RouterLink to="/page/installation">
+            Installation
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/getting-started">
+            Getting started
+        </RouterLink>
+    </li>
+    <li class="uk-nav-divider"></li>
+    <li>
+        <RouterLink to="/page/dynamic-matching">
+            Dynamic route matching
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/current-route-info">
+            Current route info
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/router-links">
+            Router links
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/programmatic-navigation">
+            Programmatic navigation
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/nested-routes">
+            Nested routes
+        </RouterLink>
+    </li>
+    <li>
+        <RouterLink to="/page/css-transitions">
+            CSS transitions
+        </RouterLink>
+    </li>
+</ul>
--- demo-app/src/Layout/MainLayout.svelte
+++ demo-app/src/Layout/MainLayout.svelte
@@ -1,5 +1,7 @@
 <script>
-    import {RouterOutlet, RouterLink} from "../../../lib";
+    import {RouterOutlet} from "../../../lib";
+    import MainMenu from "../Components/MainMenu.svelte";
+
     export let currentRoute
     $: activeRoute = currentRoute.name
 </script>
@@ -9,68 +11,12 @@
         <div class="uk-navbar-left">
             <a class="uk-navbar-item uk-logo" href="#/">Svelte Easyroute v2</a>
         </div>
-        <div class="uk-navbar-right">
-            <ul class="uk-navbar-nav">
-                <li class:uk-active={activeRoute === 'Index'}>
-                    <RouterLink to="/">
-                        Docs
-                    </RouterLink>
-                </li>
-                <li class:uk-active={activeRoute === 'Testindex'}>
-                    <RouterLink to="/login">
-                        API
-                    </RouterLink>
-                </li>
-            </ul>
-        </div>
     </nav>
     <div uk-grid style="margin-top: 2rem">
         <div class="uk-width-1-4@m">
             <div class="uk-card uk-card-default uk-card-body">
                 <div>
-                    <ul class="uk-nav uk-nav-default">
-                        <li class="uk-active">
-                            <RouterLink to="/page/installation">
-                                Installation
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/getting-started">
-                                Getting started
-                            </RouterLink>
-                        </li>
-                        <li class="uk-nav-divider"></li>
-                        <li>
-                            <RouterLink to="/page/dynamic-matching">
-                                Dynamic route matching
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/current-route-info">
-                                Current route info
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/router-links">
-                                Router links
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/programmatic-navigation">
-                                Programmatic navigation
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/nested-routes">
-                                Nested routes
-                            </RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="/page/css-transitions">
-                                CSS transitions
-                            </RouterLink>
-                        </li>
-                    </ul>
+                    <MainMenu />
                 </div>
             </div>
         </div>
