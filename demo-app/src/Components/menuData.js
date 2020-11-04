c66d56d7c32f119a5e7fb1107da4339429d6ac67
Alexey Solovyov <plotinka@lyoha.info>
2020-11-05 04:25:49 +0500

    05.11 Viewport and external links fixed

--- demo-app/src/Components/menuData.js
+++ demo-app/src/Components/menuData.js
@@ -61,7 +61,7 @@ export default {
       label: 'Рендеринг на стороне сервера'
     },
     {
-      url: '',
+      url: 'https://github.com/lyohaplotinka/svelte-easyroute-ssr-template',
       title: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                       <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
                       <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
@@ -157,7 +157,7 @@ export default {
       label: 'Server-side rendering'
     },
     {
-      url: '',
+      url: 'https://github.com/lyohaplotinka/svelte-easyroute-ssr-template',
       title: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                       <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
                       <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
